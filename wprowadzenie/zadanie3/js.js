79276e6976110c9566cef52a1b18dcaa558beb39
DESKTOP-8TS7QD0\steam <steam.korewczus@o2.pl>
2022-10-04 19:32:44 +0200

    Zadanie 3

---
+++ wprowadzenie/zadanie3/js.js
@@ -0,0 +1,16 @@
+const MojeImię = `Maciej`;
+
+if (MojeImię.includes("a")) {
+    console.log("True");
+};
+
+let MojeNazwisko = `Luśnia`;
+
+let length = MojeNazwisko.length;
+
+MojeImię.substr(0, 3);
+console.log(MojeImię.substr(0, 3));
+
+console.log(MojeImię, MojeNazwisko);
+// console.log(includes);
+console.log(length);
